f46fb0ff0d745d6f7ddae1236a45d52650671546
quincy.soeliman <quincy.soeliman@indicia.nl>
2016-03-14 15:12:13 +0100

    Created a weatherDataFactory

--- app/components/home/homeController.js
+++ app/components/home/homeController.js
@@ -7,10 +7,10 @@ angular.module('home', [
     $scope.address = '';
     $scope.location = {};
 
-    $scope.fetchLocationData = function(userInput) {
-      if (userInput != undefined) {
-        locationDataFactory.getLocationData(userInput).then(function(res) {
-          $scope.data = res.data.results[0];
+    $scope.fetchLocationData = function(location) {
+      if (location != undefined) {
+        locationDataFactory.getLocationData(location).then(function(response) {
+          $scope.data = response.data.results[0];
           $scope.address = $scope.data.formatted_address;
           $scope.location.lat = $scope.data.geometry.location.lat;
           $scope.location.lng = $scope.data.geometry.location.lng;
--- app/shared/weatherData/weatherDataFactory.js
+++ app/shared/weatherData/weatherDataFactory.js
@@ -1,4 +1,16 @@
 angular.module('weatherData', [])
   .factory('weatherDataFactory', ['$http', function($http) {
 
+    const APIKEY = 'ccf279eb1ba3481a93e236b50b2786cd';
+    var urlBase = 'https://api.forecast.io/forecast/' + apiKey;
+
+    var weatherDataFactory = {};
+
+    weatherDataFactory.getWeatherData = function($data) {
+      var $data = $data.replace(' ', '%20');
+
+      return $http.get(urlBase + '');
+    };
+
+    return weatherDataFactory;
   }]);
