9b3160285767172c4b9d74cf824052b38358a385
quincy.soeliman <quincy.soeliman@indicia.nl>
2016-03-16 10:49:38 +0100

    Retrieved all the data for the detail page

--- app/components/detail/detailController.js
+++ app/components/detail/detailController.js
@@ -8,11 +8,10 @@ angular
     $scope.locationData = {};
     $scope.weatherData = {};
 
-    $scope.address = '';
     $scope.location = {};
 
-    $scope.minTemp = '';
-    $scope.maxTemp = '';
+    // TODO: Set weatherIcon default
+    $scope.windBearing = '';
 
     $scope.fetchLocationData = function(location) {
       if (location != undefined) {
@@ -30,10 +29,62 @@ angular
     $scope.fetchWeatherData = function(latitude, longitude) {
       weatherDataFactory.getWeatherData(latitude, longitude).then(function(response) {
         // TODO: Get daily datapoint and replace data[0]
-        console.log(response);
+        console.log(response.data);
         $scope.weatherData = response.data.daily.data[0];
+
         $scope.minTemp = $scope.weatherData.temperatureMin;
         $scope.maxTemp = $scope.weatherData.temperatureMax;
-      })
+        $scope.getTemperature($scope.minTemp, $scope.maxTemp);
+
+        $scope.minApparentTemp = $scope.weatherData.apparentTemperatureMin;
+        $scope.maxApparentTemp = $scope.weatherData.apparentTemperatureMax;
+        $scope.getApparentTemperature($scope.minApparentTemp, $scope.maxApparentTemp);
+
+        $scope.summary = $scope.weatherData.summary;
+
+        $scope.weatherIcon = $scope.weatherData.icon;
+
+        $scope.windSpeed = $scope.weatherData.windSpeed;
+        $scope.humidity = $scope.weatherData.humidity;
+        $scope.getWindBearing($scope.weatherData.windBearing);
+      });
+    };
+
+    $scope.getTemperature = function(minTemp, maxTemp) {
+      $scope.temperature = Math.round((minTemp + maxTemp) / 2);
+    };
+
+    $scope.getApparentTemperature = function(minApparentTemp, maxApparentTemp) {
+      $scope.apparentTemp = Math.round((minApparentTemp + maxApparentTemp) / 2);
+    };
+
+    $scope.getWindBearing = function(windBearing) {
+      switch (true) {
+        case (windBearing == 0):
+        default:
+          $scope.windBearing = 'north';
+          break;
+        case (windBearing > 0 && windBearing < 90):
+          $scope.windBearing = 'north-east';
+          break;
+        case (windBearing == 90):
+          $scope.windBearing = 'east';
+          break;
+        case (windBearing > 90 && windBearing < 180):
+          $scope.windBearing = 'south-east';
+          break;
+        case (windBearing == 180):
+          $scope.windBearing = 'south';
+          break;
+        case (windBearing > 180 && windBearing < 270):
+          $scope.windBearing = 'south-west';
+          break;
+        case (windBearing == 270):
+          $scope.windBearing = 'west';
+          break;
+        case (windBearing > 270 && windBearing < 360):
+          $scope.windBearing = 'north-west';
+          break;
+      }
     };
   }]);
